b3c2167b1544754a51067d30d32c825b96014e69
blueskyfre <chphed@gmail.com>
2026-04-19 22:27:48 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+ /* 
+  html 파일에서만 공통적으로 사용 - configna.gs 파일도 동시에 수정해야 함
+  SS_ID : 스프레드시트 ID, DEPLOY : 배포주소, GITHUB : 깃허브주소
+ */
+  
+  
+  var Config = (function() {
+  'use strict';
+  return {
+    SS_ID  : '1QUUKQpDxTkJIp53UAptP3Zq6PrOSwJURYPAKV-E4Ti8',
+    DEPLOY : 'https://script.google.com/macros/s/AKfycbzJQQTEhSSGAilKc2KBk1n56jPOJE1H4Qbm72cN0vF8WoOCibp59WgPSHXo26Y_8NeZnw/exec',
+    GITHUB : 'https://blueskyfre.github.io/chphed.github.io'
+  };
+})();
+
+
