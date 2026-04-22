c7d1444c3abb0acf85ba69df71d6a2f0f5114398
blueskyfre <chphed@gmail.com>
2026-04-23 06:45:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
- /* 
+/* 
   html 파일에서만 공통적으로 사용 - configna.gs 파일도 동시에 수정해야 함
-  SS_ID : 스프레드시트 ID, DEPLOY : 배포주소, GITHUB : 깃허브주소
- */
-  
-  
-  var Config = (function() {
+  SS_ID : 스프레드시트 ID, DEPLOY : 배포주소, GITHUB : 깃허브주소, BACKGROUND_IMG : 배경 이미지 주소
+*/
+
+var Config = (function() {
   'use strict';
   return {
-    SS_ID  : '1QUUKQpDxTkJIp53UAptP3Zq6PrOSwJURYPAKV-E4Ti8',
-    DEPLOY : 'https://script.google.com/macros/s/AKfycbzJQQTEhSSGAilKc2KBk1n56jPOJE1H4Qbm72cN0vF8WoOCibp59WgPSHXo26Y_8NeZnw/exec',
-    GITHUB : 'https://blueskyfre.github.io/chphed.github.io'
+    SS_ID          : '1QUUKQpDxTkJIp53UAptP3Zq6PrOSwJURYPAKV-E4Ti8',
+    DEPLOY         : 'https://script.google.com/macros/s/AKfycbzJQQTEhSSGAilKc2KBk1n56jPOJE1H4Qbm72cN0vF8WoOCibp59WgPSHXo26Y_8NeZnw/exec',
+    GITHUB         : 'https://blueskyfre.github.io/chphed.github.io',
+    BACKGROUND_IMG : 'https://github.com/blueskyfre/chphed.github.io/raw/main/back.png'
   };
 })();
-
 
